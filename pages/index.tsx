72002d10a23c36dec493030eaf0cb2b367db0219
Aid Thompson <thePandaRiot@gmail.com>
2020-08-14 22:13:53 +0100

    very basic face in working with gsap

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,59 @@
-import React, { ReactElement, useEffect, useState } from 'react';
+import React, { ReactElement, useEffect, useState, useRef } from 'react';
 import Head from 'next/head';
 import { RichText } from 'prismic-reactjs';
+import { useIntersection } from 'react-use';
 import { useQuery } from 'react-query';
 import { fetchPageByUID } from '../api/requests';
 import { Hero } from '../components';
 import ResponsiveImage from '../components/responsive-image';
+import gsap from 'gsap';
 
 interface Props {
 	ssrContent: object;
 }
 
 function Home({ ssrContent }: Props): ReactElement {
-	
 	// @ts-ignore
 	const { data, isLoading } = useQuery('content', fetchPageByUID, { initialData: ssrContent, enabled: 'home-page' });
 
+	let sectionRef = useRef(null);
+
+	const intersection = useIntersection(sectionRef, {
+		root: null,
+		rootMargin: '0px', // controls where the animation starts. 400px half way, 0px the end of the section.
+		threshold: 1, // when we reach entire div, 1 = 100%, calls div to run.
+		// threshold should be the same as intersectionRatio vv
+	});
+	const fadeIn = (element) => {
+		if (process.browser) {
+			gsap.to(element, 1, {
+				opacity: 1,
+				y: -60,
+				ease: 'Power4.out',
+				stagger: {
+					amount: 0.3,
+				},
+			});
+		}
+	};
+
+	const fadeOut = (element) => {
+		if (process.browser) {
+			gsap.to(element, 1, {
+				opacity: 0,
+				y: -20,
+				ease: 'Power4.out',
+			});
+		}
+	};
+
 	if (isLoading) {
-		return <p>is loading is true...</p>
+		return <p>is loading is true...</p>;
 	}
+
 	if (ssrContent) {
+		intersection && intersection.intersectionRatio < 0.5 ? fadeOut('.fadeIn') : fadeIn('.fadeIn');
+
 		return (
 			<div className="container">
 				<Head>
@@ -26,7 +61,7 @@ function Home({ ssrContent }: Props): ReactElement {
 					<link rel="icon" href="/favicon.ico" />
 
 					<meta name="twitter:card" content="summary_large_image" />
-					
+
 					<meta name="twitter:image" content={data.data['twitter_image'].url} />
 					<meta name="twitter:creator" content="@aidThompsin" />
 					<meta name="twitter:site" content="@funkTwentySeven" />
@@ -34,7 +69,6 @@ function Home({ ssrContent }: Props): ReactElement {
 					<meta property="og:title" content={`Funk-27 | ${data.data.title[0].text}`} key="title" />
 					<meta property="og:description" content={data.data.first_section[0].text} key="description" />
 					<meta property="og:image" content={data.data.twitter_image.url} key="seo share image" />
-
 				</Head>
 
 				<main>
@@ -47,17 +81,19 @@ function Home({ ssrContent }: Props): ReactElement {
 						/>
 					</section>
 
-					<section>
-						<RichText
-							// @ts-ignore
-							render={data.data.first_section}
-						/>
+					<section ref={sectionRef}>
+						<div className="fadeIn">
+							<RichText
+								// @ts-ignore
+								render={data.data.first_section}
+							/>
+						</div>
 					</section>
 				</main>
 			</div>
 		);
 	} else {
-		return <p>LoadingSpinner</p>
+		return <p>LoadingSpinner</p>;
 	}
 }
 
