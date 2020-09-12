f26819470830f4062f0db3666fdb09ff1c5c9be9
Aid Thompson <thePandaRiot@gmail.com>
2020-09-12 06:03:29 +0100

    added basic unfinished blog article page

--- api/requests.ts
+++ api/requests.ts
@@ -1,20 +1,14 @@
 import { useQuery } from 'react-query';
 import Prismic from 'prismic-javascript';
 
-
 const linkResolver = function(doc) {
-	// Pretty URLs for known types
-	if (doc.type === 'blog') return "/post/" + doc.slug;
+	if (doc.type === 'blog') return "/blog/" + doc.slug;
 	if (doc.type === 'page') return "/" + doc.slug;
 	// Fallback for other types, in case new custom types get created
 	return "/doc/" + doc.id;
 };
 
 async function fetchPageByUID(key, uid) {
-	
-	// console.log('AT | fetchPageByUID key:', key);
-	// console.log('AT | fetchPageByUID uid:', uid);
-
 	return Prismic.getApi('https://funk27.cdn.prismic.io/api/v2')
 		.then((api) => api.getByUID('page', uid,  { 'fetchLinks': ['blog-page.blog-title', 'blog-page.blog-body', 'blog-page.blog-image-1', 'blog-page.blog-image-2'] }),
         )
@@ -25,6 +19,16 @@ async function fetchPageByUID(key, uid) {
 		.catch((err) => console.log('AT | err :', err));
 }
 
+
+async function fetchBlogPageByUID(key, uid) {
+	return Prismic.getApi('https://funk27.cdn.prismic.io/api/v2')
+		.then(api => api.getByUID('blog-page', uid))
+		.then(response => response)
+		.catch(err => console.log('blog fetch error: ', err));
+}
+
+
+
 const useContent = (key, uid) => {
 	return useQuery([key, uid], fetchPageByUID, {
 		enabled: uid,
@@ -32,4 +36,4 @@ const useContent = (key, uid) => {
 	});
 };
 
-export { useContent, fetchPageByUID };
+export { useContent, fetchPageByUID, fetchBlogPageByUID };
--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,16 +1,54 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { fetchBlogPageByUID, useContent } from '../../api/requests';
+import { RichText } from 'prismic-reactjs';
 
 interface Props {
-
+	ssrContent: object;
 }
 
-function BlogArticle({}: Props): ReactElement {
+function BlogArticle({ ssrContent }: Props): ReactElement {
+	const [content, setContent] = useState(ssrContent);
+	const router = useRouter();
+
+	useEffect(() => {
+		console.log(router);
+		if (!content) {
+			const { data, isLoading } = useContent('content', router.query.slug);
+			//@ts-ignore
+			setContent(data);
+		}
+	}, [content]);
+
+	if (content) {
+		console.log('content is here: ', content);
 
 		return (
-			<div className="blog__home container">
-                <h1>i am a blog article [placeholder]</h1>
+			<div className="blog__article container">
+				<main>
+					<section>
+						<div className="columns is-multiline">
+							{
+								//@ts-ignore
+								RichText.render(content.data['blog-title'])
+							}
+
+							<hr className="blog-article__divider" />
+
+							<RichText
+								//@ts-ignore
+								render={content.data['blog-body']} />
+						</div>
+					</section>
+				</main>
 			</div>
 		);
-};
+	}
+}
+
+export async function getServerSideProps(context: any) {
+	const ssrContent = await fetchBlogPageByUID('content', context.params.slug);
+	return { props: { ssrContent } };
+}
 
 export default BlogArticle;
